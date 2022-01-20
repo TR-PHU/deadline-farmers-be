de3dcce3d1003936595946fe5f0db20c9347bccd
cawfee4 <cawfee4@gmail.com>
2022-01-20 17:44:06 +0700

    thay doi signup thanh forgetpassword

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const authService = require('../services/auth.service');
 
 module.exports = {
     register: () => {},
-    signUp: () => {},
     signIn: () => {},
+    forgetPassword: () => {},
     resetPassword: () => {},
 };
--- src/api/routes/auth.route.js
+++ src/api/routes/auth.route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 router.post('/register', authController.register);
-router.post('/sign-up', authController.signUp);
 router.post('/sign-in', authController.signIn);
+router.post('/forget-password', authController.forgetPassword);
 router.post('/reset-password', authController.resetPassword);
 module.exports = router;

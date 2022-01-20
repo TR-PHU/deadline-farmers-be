564b9ddd20493b99be092446a9b333f3a8adc797
cawfee4 <cawfee4@gmail.com>
2022-01-20 17:35:14 +0700

    module export o user.js

--- src/api/models/user.js
+++ src/api/models/user.js
@@ -1,28 +1,33 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            minlength: 5,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            minlength: 5,
+            require: true,
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+        },
     },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    username: {
-        type: String,
-        minlength: 5,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        minlength: 5,
-        require: true
-    },
-    email: {
-        type: String,
-        require: true,
-        unique: true
-    }
-}, { timestamps: true });
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('user', UserSchema);

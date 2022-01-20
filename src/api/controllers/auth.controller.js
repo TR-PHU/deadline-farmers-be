69b8572c5829cde34d88d166343e169ee6fa207c
cawfee4 <cawfee4@gmail.com>
2022-01-20 20:46:22 +0700

    sign in

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -2,7 +2,14 @@ const authService = require('../services/auth.service');
 
 module.exports = {
     register: () => {},
-    signIn: () => {},
+    signIn: async (req, res, next) => {
+        try {
+            let DTO = await authService.signIn(req.body);
+            res.status(200).json(DTO);
+        } catch (error) {
+            next(error);
+        }
+    },
     forgetPassword: () => {},
     resetPassword: () => {},
 };
--- src/api/services/auth.service.js
+++ src/api/services/auth.service.js
@@ -1,1 +1,27 @@
+const createError = require('http-errors');
+const db = require('../configs/db.config');
 
+module.exports = {
+    signIn: async ({ username, password }) => {
+        try {
+            let filterUser = db.findOne({ username: username });
+            console.log(filterUser);
+            const accessToken = jwt.sign(
+                {
+                    email: filterUser[0].email,
+                },
+                'secret'
+            );
+
+            const refreshToken = jwt.sign(
+                {
+                    email: filterUser[0].email,
+                    accessToken: accessToken,
+                },
+                'secret'
+            );
+        } catch (error) {
+            throw new createError(500, 'Can not login');
+        }
+    },
+};

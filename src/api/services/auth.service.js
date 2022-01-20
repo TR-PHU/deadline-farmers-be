4b34fb5f34d1fc4f3a37f2234fd8f7484291feeb
cawfee4 <cawfee4@gmail.com>
2022-01-20 22:25:00 +0700

    sign in redo

--- src/api/services/auth.service.js
+++ src/api/services/auth.service.js
@@ -5,73 +5,76 @@ const sendMail = require('../../commons/emails/sendMail');
 const { v4: uuidv4 } = require('uuid');
 const jwt = require('jsonwebtoken');
 
-const Register = async (body) => {
-    try {
-        const { username, password, email, address, phone, fullname } = body;
-        const user_Name = await User.find({ username: username });
-        const user_email = await User.find({ email: email });
-        if (user_Name.length > 0) {
-            throw new createError(400, 'Username already exist!');
-        }
-        if (user_email.length > 0) {
-            throw new createError(400, 'Your email already exist!');
-        }
-        try {
-            const salt = await bcrypt.genSalt(10);
-            const hashPassword = await bcrypt.hash(password, salt);
-
-            const responseDB = await User.create({
-                username,
-                password: hashPassword,
-                email,
-                address,
-                phone,
-                fullname,
-            });
-
-            console.log('Response Database:', responseDB);
-            return responseDB;
-        } catch (error) {
-            throw error;
-        }
-    } catch (error) {
-        if (error) {
-            throw error;
-        }
-        throw new createError(500, 'Cannot create User');
-    }
-};
-
-const GetAllUsers = async () => {
-    try {
-        const res = await User.find();
-        return res;
-    } catch (error) {
-        throw new createError(500, 'Cannot get all users!');
-    }
-};
-
 module.exports = {
-    Register,
-    GetAllUsers,
+    Register: async (body) => {
+        try {
+            const { username, password, email, address, phone, fullname } = body;
+            const user_Name = await User.find({ username: username });
+            const user_email = await User.find({ email: email });
+            if (user_Name.length > 0) {
+                throw new createError(400, 'Username already exist!');
+            }
+            if (user_email.length > 0) {
+                throw new createError(400, 'Your email already exist!');
+            }
+            try {
+                const salt = await bcrypt.genSalt(10);
+                const hashPassword = await bcrypt.hash(password, salt);
+
+                const responseDB = await User.create({
+                    username,
+                    password: hashPassword,
+                    email,
+                    address,
+                    phone,
+                    fullname,
+                });
+
+                console.log('Response Database:', responseDB);
+                return responseDB;
+            } catch (error) {
+                throw error;
+            }
+        } catch (error) {
+            if (error) {
+                throw error;
+            }
+            throw new createError(500, 'Cannot create User');
+        }
+    },
+    GetAllUsers: async () => {
+        try {
+            const res = await User.find();
+            return res;
+        } catch (error) {
+            throw new createError(500, 'Cannot get all users!');
+        }
+    },
     signIn: async ({ username, password: plainPassword }) => {
         try {
             let filterUser = await User.find({ username: username });
             if (filterUser.length === 1) {
-                const accessToken = jwt.sign(
-                    {
-                        username: filterUser[0].username,
-                    },
-                    'secret'
-                );
-                const refreshToken = jwt.sign(
-                    {
-                        username: filterUser[0].username,
-                        accessToken: accessToken,
-                    },
-                    'secret'
-                );
                 if (await bcrypt.compare(plainPassword, filterUser[0].password)) {
+                    const accessToken = jwt.sign(
+                        {
+                            userID: filterUser[0]._id,
+                            username: filterUser[0].username,
+                        },
+                        process.env.JWT_SERCET,
+                        {
+                            expiresIn: '3h',
+                        }
+                    );
+                    const refreshToken = jwt.sign(
+                        {
+                            userID: filterUser[0]._id,
+                            username: filterUser[0].username,
+                        },
+                        process.env.JWT_SERCET,
+                        {
+                            expiresIn: '5h',
+                        }
+                    );
                     return {
                         error: false,
                         msg: 'Login success',

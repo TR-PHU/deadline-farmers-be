c0531be88c1e9f0ac7ff02c84f62cf0dc0901411
cawfee4@gmail.com <cawfee4@gmail.com>
2022-01-23 00:36:40 +0800

    delete file in folder while uploading to db

--- src/api/routes/product.route.js
+++ src/api/routes/product.route.js
@@ -1,21 +1,22 @@
-const router = require("express").Router();
-const productController = require("../controllers/product.controller");
-const { verifyToken } = require("../middlewares/verifyToken");
-const { verifyAdmin } = require("../middlewares/verifyAdmin");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const router = require('express').Router();
+const productController = require('../controllers/product.controller');
+const { verifyToken } = require('../middlewares/verifyToken');
+const { verifyAdmin } = require('../middlewares/verifyAdmin');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 //get all product
-router.get("/", productController.getAllProduct);
+router.get('/', productController.getAllProduct);
 //create product(Admin role)
 router.post('/', verifyToken, verifyAdmin, upload.single('image'), productController.createProduct);
 //delete product
-router.delete(
-  "/:id",
-  verifyToken,
-  verifyAdmin,
-  productController.deleteProduct
-);
+router.delete('/:id', verifyToken, verifyAdmin, productController.deleteProduct);
 //update product
-router.put('/:id', verifyToken, verifyAdmin, upload.single("image"),productController.updateProductById);
+router.put(
+    '/:id',
+    verifyToken,
+    verifyAdmin,
+    upload.single('image'),
+    productController.updateProductById
+);
 
 module.exports = router;

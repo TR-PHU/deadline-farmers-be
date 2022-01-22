7a885afcb56460e02c5513c694a896c31e335078
TR-PHU <tranphutptv@gmail.com>
2022-01-22 10:02:39 +0700

    Update file verifyToken

--- src/api/services/cart.service.js
+++ src/api/services/cart.service.js
@@ -6,19 +6,18 @@ module.exports = {
             if (products.length == 0) {
                 const res = await Cart.deleteOne({ userId });
                 console.log(res);
-                if(res.deletedCount == 1) {
+                if (res.deletedCount == 1) {
                     return {
                         statusCode: 200,
-                        msg: 'Ok'
-                    }
-                }else {
-                    throw new createError(500, "Interval server errors");
+                        msg: 'Ok',
+                    };
+                } else {
+                    throw new createError(500, 'Interval server errors');
                 }
             }
 
             const findCart = await Cart.find({ userId });
             if (findCart.length > 0) {
-<<<<<<< HEAD
                 const productsList = findCart[0].products.concat(products);
 
                 const res = await Cart.updateOne(
@@ -30,15 +29,6 @@ module.exports = {
                     }
                 );
 
-=======
-                const res = await Cart.updateOne(
-                    { userId },
-                    {
-                        $set: { products }
-                    }
-                );
-                console.log(res);
->>>>>>> 049455f62a469fe0c0f4511c330dcbcccc9bc954
                 return {
                     statusCode: 201,
                     msg: 'ok',

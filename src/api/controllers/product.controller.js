9f0cd55ba74c2aa8435d99447b793bfb2315720b
cawfee4@gmail.com <cawfee4@gmail.com>
2022-01-22 22:49:40 +0800

    change categories into category

--- src/api/controllers/product.controller.js
+++ src/api/controllers/product.controller.js
@@ -1,47 +1,50 @@
-const productService = require('../services/product.service');
+const productService = require("../services/product.service");
 module.exports = {
-    getProductById: async (req, res, next) => {
-        try {
-            const DTO = await productService.getProductById(req.params.id);
+  getProductById: async (req, res, next) => {
+    try {
+      const DTO = await productService.getProductById(req.params.id);
 
-            res.status(200).json(DTO);
-        } catch (error) {
-            next(error);
-        }
-    },
-    createProduct: async (req, res, next) => {
-        try {
-            const DTO = await productService.CreateProduct(req);
-            res.status(DTO.statusCode).json({ message: DTO.msg });
-        } catch (error) {
-            next(error);
-        }
-    },
-    getAllProduct: async (req, res, next) => {
-        try {
-            let qPage = req.query.page;
-            const DTO = await productService.getAllProduct(qPage);
-            res.status(200).json(DTO);
-        } catch (error) {
-            next(error);
-        }
-    },
-    deleteProduct: async (req, res, next) => {
-        try {
-            const DTO = await productService.deleteProductById(req.params.id);
+      res.status(200).json(DTO);
+    } catch (error) {
+      next(error);
+    }
+  },
+  createProduct: async (req, res, next) => {
+    try {
+      const DTO = await productService.CreateProduct(req);
+      res.status(DTO.statusCode).json({ message: DTO.msg });
+    } catch (error) {
+      next(error);
+    }
+  },
+  getAllProduct: async (req, res, next) => {
+    try {
+      let qPage = req.query.page;
+      const DTO = await productService.getAllProduct(qPage);
+      res.status(200).json(DTO);
+    } catch (error) {
+      next(error);
+    }
+  },
+  deleteProduct: async (req, res, next) => {
+    try {
+      const DTO = await productService.deleteProductById(req.params.id);
 
-            return res.json(DTO);
-        } catch (error) {
-            next(error);
-        }
-    },
-    updateProductById: async (req, res, next) => {
-        try {
-            const DTO = await productService.updateProductById(req.params.id, req.body);
+      return res.json(DTO);
+    } catch (error) {
+      next(error);
+    }
+  },
+  updateProductById: async (req, res, next) => {
+    try {
+      const DTO = await productService.updateProductById(
+        req.params.id,
+        req.body
+      );
 
-            res.status(200).json(DTO);
-        } catch (error) {
-            next(error);
-        }
-    },
+      res.status(200).json(DTO);
+    } catch (error) {
+      next(error);
+    }
+  },
 };
--- src/api/services/product.service.js
+++ src/api/services/product.service.js
@@ -1,116 +1,113 @@
-const Product = require('../models/product');
-const CreateError = require('http-errors');
-const createError = require('http-errors');
-const cloudinary = require('../configs/cloudinary.config');
-const mongoose = require('mongoose');
-const upload = require('../configs/multer.config');
+const Product = require("../models/product");
+const CreateError = require("http-errors");
+const createError = require("http-errors");
+const cloudinary = require("../configs/cloudinary.config");
+const mongoose = require("mongoose");
+const upload = require("../configs/multer.config");
 module.exports = {
-    getProductById: async (productId) => {
-        try {
-            if (!mongoose.Types.ObjectId.isValid(productId)) {
-                throw new createError(404, 'Product not found!');
-            }
-            const res = await Product.find({ _id: productId });
-            console.log(res);
-            if (!res) {
-                throw new CreateError(404, 'Product not found!');
-            }
-            return res;
-        } catch (error) {
-            if (error) throw error;
-            throw new CreateError(500, 'Internal server errors');
-        }
-    },
-    CreateProduct: async (req) => {
-        try {
-            const { name, price, categories, quantity, description, rating } = req.body;
-            const result = await cloudinary.uploader.upload(req.file.path);
-            let product = new Product({
-                name: name,
-                image: result.secure_url,
-                cloudinary_id: result.public_id,
-                price: price,
-                categories: categories,
-                quantity: quantity,
-                description: description,
-                rating: rating,
-            });
-            console.log(result)
-            //Save product
-            await product.save();
-            return {
-                statusCode: 200,
-                msg: 'Successfully created a product',
-            };
-        } catch (error) {
-            throw new CreateError(500, 'Internal server errors');
-        }
-    },
-    getAllProduct: async (qPage) => {
-        const PAGE_SIZE = 12;
-        try {
-            if (qPage) {
-                qPage = parseInt(qPage);
-                qPage < 1 ? (qPage = 1) : qPage;
-                const startIndex = (qPage - 1) * PAGE_SIZE;
-                const products = await Product.find().skip(startIndex).limit(PAGE_SIZE);
-                return {
-                    statusCode: 200,
-                    products,
-                };
-            } else {
-                const products = await Product.find();
-                return {
-                    statusCode: 200,
-                    products,
-                };
-            }
-        } catch (error) {
-            throw new CreateError(error);
-        }
-    },
-    deleteProductById: async (id) => {
-        try {
-            const resDB = await Product.findById(id);
-            console.log('as')
-            const resCloud = await cloudinary.uploader.destroy(resDB[0].cloudinary_id);
+  getProductById: async (productId) => {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(productId)) {
+        throw new createError(404, "Product not found!");
+      }
+      const res = await Product.find({ _id: productId });
+      console.log(res);
+      if (!res) {
+        throw new CreateError(404, "Product not found!");
+      }
+      return res;
+    } catch (error) {
+      if (error) throw error;
+      throw new CreateError(500, "Internal server errors");
+    }
+  },
+  CreateProduct: async (req) => {
+    try {
+      const { name, price, categories, quantity, description, rating } =
+        req.body;
+      const result = await cloudinary.uploader.upload(req.file.path);
+      let product = new Product({
+        name: name,
+        image: result.secure_url,
+        cloudinary_id: result.public_id,
+        price: price,
+        categories: categories,
+        quantity: quantity,
+        description: description,
+        rating: rating,
+      });
+      //Save product
+      await product.save();
+      return {
+        statusCode: 200,
+        msg: "Successfully created a product",
+      };
+    } catch (error) {
+      throw new CreateError(500, "Internal server errors");
+    }
+  },
+  getAllProduct: async (qPage) => {
+    const PAGE_SIZE = 12;
+    try {
+      if (qPage) {
+        qPage = parseInt(qPage);
+        qPage < 1 ? (qPage = 1) : qPage;
+        const startIndex = (qPage - 1) * PAGE_SIZE;
+        const products = await Product.find().skip(startIndex).limit(PAGE_SIZE);
+        return {
+          statusCode: 200,
+          products,
+        };
+      } else {
+        const products = await Product.find();
+        return {
+          statusCode: 200,
+          products,
+        };
+      }
+    } catch (error) {
+      throw new CreateError(error);
+    }
+  },
+  deleteProductById: async (id) => {
+    try {
+      const res = await Product.deleteOne({ _id: id });
 
-            console.log(resCloud);
-
-            await resDB.remove()
-            return {
-                statusCode: 202,
-                msg: 'Delete Success!',
-            };
-        } catch (error) {
-            throw new CreateError(500, 'Internal server errors');
+      console.log(res);
+      return {
+        statusCode: 202,
+        msg: "Delete Success!",
+      };
+    } catch (error) {
+      throw new CreateError(500, "Internal server errors");
+    }
+  },
+  updateProductById: async (id, body) => {
+    try {
+      const { name, description, image, price, quantity, rating, categories } =
+        body;
+      console.log(categories);
+      const res = await Product.updateOne(
+        { _id: id },
+        {
+          $set: {
+            name,
+            description,
+            image,
+            price,
+            quantity,
+            rating,
+            categories,
+          },
         }
-    },
-    updateProductById: async (id, body) => {
-        try {
-            const { name, description, image, price, quantity, rating, catagories } = body;
-            console.log(catagories);
-            const res = await Product.updateOne(
-                { _id: id },
-                {
-                    $set: {
-                        name,
-                        description,
-                        image,
-                        price,
-                        quantity,
-                        rating,
-                        catagories,
-                    },
-                }
-            );
+      );
 
-            return {
-                statusCode: 204,
-                msg: 'ok',
-            };
-        } catch (error) {
-            throw new CreateError(500, 'Interval server errors');
-        }
-    },
+      return {
+        statusCode: 204,
+        msg: "ok",
+      };
+    } catch (error) {
+      throw new CreateError(500, "Interval server errors");
+    }
+  },
 };

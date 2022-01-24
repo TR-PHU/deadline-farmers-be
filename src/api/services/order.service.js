3c5ca014ff2e47f2dc6829a5a4ee6c7be4c0a47b
cawfee4@gmail.com <cawfee4@gmail.com>
2022-01-24 20:08:15 +0700

    fix getOrder bug not returning product

--- src/api/services/order.service.js
+++ src/api/services/order.service.js
@@ -1,50 +1,52 @@
-const createError = require('http-errors');
-const Order = require('../models/order');
-const Product = require('../models/product')
+const createError = require("http-errors");
+const Order = require("../models/order");
+const Product = require("../models/product");
 
 module.exports = {
-    addOrder: async (userId, products) => {
-        try {
-            // const newOrder = new Order({ userId, products });
-            // await newOrder.save();
-            const order = await Order.updateOne({ userId}, {
-                $push: {
-                    products: { $each: [...products]}
-                }
-            });
-            console.log(order);
-
-            return {
-                statusCode: 200,
-                message: 'Payment success',
-            };
-        } catch (error) {
-            if(error) throw new createError(error);
-            throw new createError(500, "Interval server errors")
+  addOrder: async (userId, products) => {
+    try {
+      const order = await Order.updateOne(
+        { userId },
+        {
+          $push: {
+            products: { $each: [...products] },
+          },
         }
-    },
-    getOrder: async (Id) => {
-        try {
-            let res = await Order.find({ userId: Id });
-            if (res.length === 0) {
-                return [];
-            }
-
-            for (let i of res[0].products) {
-                const resDB = await Product.findById(i.productId);
-                if (resDB) {
-                    i.name = resDB.name;
-                    i.image = resDB.image;
-                }
-            }
-
-            return {
-                statusCode: 200,
-                message: 'Getting order successful',
-                order: res,
-            };
-        } catch (error) {
-            throw new createError(error);
+      );
+      return {
+        statusCode: 200,
+        message: "Payment success",
+      };
+    } catch (error) {
+      if (error) throw new createError(error);
+      throw new createError(500, "Interval server errors");
+    }
+  },
+  getOrder: async (Id) => {
+    try {
+      let res = await Order.find({ userId: Id });
+      if (res.length === 0) {
+        return [];
+      }
+      let count = 0;
+      let resDB = [];
+      for (let i of res[0].products) {
+        resDB[count] = await Product.findById(i.productId);
+        if (resDB) {
+          i.name = resDB.name;
+          i.image = resDB.image;
         }
-    },
+        x++;
+        if (count === res[0].products.length) {
+          return {
+            statusCode: 200,
+            message: "Getting order successful",
+            resDB,
+          };
+        }
+      }
+    } catch (error) {
+      throw new createError(error);
+    }
+  },
 };

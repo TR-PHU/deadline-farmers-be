be34f4e97a79622bf174ad215e14488bcb9dc5c8
TR-PHU <tranphutptv@gmail.com>
2022-01-24 22:32:32 +0700

    fix add order

--- src/api/services/order.service.js
+++ src/api/services/order.service.js
@@ -1,4 +1,5 @@
 const createError = require('http-errors');
+const { log } = require('util');
 const Order = require('../models/order');
 const Product = require('../models/product');
 
@@ -18,6 +19,10 @@ module.exports = {
                     },
                 }
             );
+            if (!order.modifiedCount) {
+                const newOrder = new Order({ userId, products });
+                await newOrder.save();
+            }
             return {
                 statusCode: 200,
                 message: 'Payment success',
@@ -27,33 +32,15 @@ module.exports = {
             throw new createError(500, 'Interval server errors');
         }
     },
-    getOrder: async(id) => {
+    getOrder: async (id) => {
         try {
             let res = await Order.find({ userId: id });
 
+            console.log(res);
             if (res.length == 0) {
                 return [];
             }
-            let count = 0;
-            let resDB = [];
 
-            for (let i of res[0].products) {
-                let x = await Product.findById(i.productId);
-                if (x !== null) {
-                    resDB[count] = x;
-                    resDB[count].quantity = i.quantity;
-                    resDB[count].price = i.price;
-                    count++;
-                }
-                if (count === res[0].products.length) {
-                    return {
-                        statusCode: 200,
-                        message: 'Getting order successful',
-                        order: resDB,
-                    };
-                }
-            }
-            
             return res;
         } catch (error) {
             throw new createError(error);
